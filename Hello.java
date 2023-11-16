3f4cb13d98ea998206ace8d0b2f9f74a913d6dd3
a-lazy-man <129570530+a-lazy-man@users.noreply.github.com>
2023-11-16 11:29:39 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello{
     public static void main(String[] args) {
         System.out.println("hello github!!!");
         System.out.println("aaaaaaaaa");
+        Integer age =23;
+        String name ="盛双健";
     }
 }
